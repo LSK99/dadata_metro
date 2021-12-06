8b88f09cd9dcd837e22fd8ca8bd33d4558f22a2b
LSK99 <lobanova.sk99>
2021-12-06 18:20:56 +0300

    init commit

---
+++ lib/dadata_metro.rb
@@ -0,0 +1,6 @@
+require "dadata_metro/version"
+
+module DadataMetro
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/dadata_metro/version.rb
@@ -0,0 +1,3 @@
+module DadataMetro
+  VERSION = "0.1.0"
+end
